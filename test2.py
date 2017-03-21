b6fd78a6dd7bb1fe3cd7cff30014966036b73475
Petr Kozyrev <kozyrevpa@gmail.com>
2017-03-22 00:16:24 +0300

    fix

--- test2.py
+++ test2.py
@@ -1,11 +1,12 @@
-with open('/file1') as uncl, \
-     open('/file2') as iem, \
-     open('/file3', 'w') as f:
-    for line in uncl:
-        line = line.strip()
-        for line2 in iem:
-            line2 = line2.strip()
-            if line in line2:
-                f.write(line)
-                f.write(next(iem))
+from tqdm import tqdm
 
+with open('/home/petr/master/classifier/unclassified_ids_1-48.txt') as uncl, \
+     open('/home/petr/master/classifier/unclass_1-38.fa', 'w') as f:
+    for line in tqdm(uncl):
+        line = line.strip()
+        with open('/home/petr/master/metagenome_samples/low_merge/stitched_reads/K1_IEM-38_S36_L001.assembled.fastq') as iem:
+            for line2 in iem:
+                line2 = line2.strip()
+                if line in line2:
+                    f.write(line + '\n')
+                    f.write(next(iem) + '\n')
